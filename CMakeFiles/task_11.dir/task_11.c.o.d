eb4d93d5f1d8cf63f8acf5965bcadb4bef63b9d1
Hugo Lindfors <hugo.lindfors2@outlook.com>
2025-12-16 16:40:09 +0100

    fixes

--- CMakeFiles/task_11.dir/task_11.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/task_11.dir/task_11.c.o: \
-  /Users/hugolindfors/Documents/oru/dt501g/inlupp1/task_11.c
---
+++ task_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("Magnus Magnusson\n");
+        printf("Magnus.Magnusson@email.tld\n");
+        printf("Spela ukelele\n");
+        return 0;
+}
